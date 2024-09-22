78849c6b2c5017365747d9a29ec18d3e4060cdc5
Satyam Singhal <satyam.singhal02.07@gmail.com>
2024-09-22 21:44:51 +0530

    Final

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ scalar = pickle.load(open('scaler.pkl','rb'))
 def homepage():
     return render_template('home.html')
 
-@app.route('/predict',methods = ['POST'])
-def predict():
+@app.route('/predict_api',methods = ['POST'])
+def predict_api():
     data = request.json['data']
     print(data)
     data_df = pd.DataFrame([data])
@@ -22,6 +22,14 @@ def predict():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    input_data = scalar.transform(np.array(data).reshape(1,-1))
+    output = model.predict(input_data)
+    return render_template("home.html",prediction_text = "The House Price Prediction is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
